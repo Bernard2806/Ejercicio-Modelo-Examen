d3cf2f3806699f8a19349cba2cd05f6de643759f
INFO-2 <INFO-2@DESKTOP-7JGV2HI>
2024-05-29 22:03:08 -0300

    FINISHEDDDDDDDD TERMINADOOOOOOOOOOOOOOOOOO

--- Modelo Examen/Program.cs
+++ Modelo Examen/Program.cs
@@ -40,9 +40,9 @@ namespace Modelo_Examen
             } while (true);
         }
 
-            private static void ImprimirMenuInicial()
-            {
-                string MenuIncio = @"
+        private static void ImprimirMenuInicial()
+        {
+            string MenuIncio = @"
  _      ____  ____  _____ _     ____    ________  _ ____  _      _____ _     
 / \__/|/  _ \/  _ \/  __// \   /  _ \  /  __/\  \///  _ \/ \__/|/  __// \  /|
 | |\/||| / \|| | \||  \  | |   | / \|  |  \   \  / | / \|| |\/|||  \  | |\ ||
@@ -50,169 +50,168 @@ namespace Modelo_Examen
 \_/  \|\____/\____/\____\\____/\____/  \____\/__/\\\_/ \|\_/  \|\____\\_/  \|
                                                                              
 ";
-                Console.WriteLine(MenuIncio);
-                Console.WriteLine("Presione cualquier tecla para continuar...");
-                Console.ReadKey();
-                Console.Clear();
-            }
-
-            private static Escuela CargarEscuela()
-            {
-                Escuela escuela = new Escuela();
-
-                Console.WriteLine("Ingrese el Nombre de la Institución: ");
-                escuela.NombreEsc = Console.ReadLine();
-                if (string.IsNullOrWhiteSpace(escuela.NombreEsc)) return null;
-
-                Console.WriteLine("Ingrese la Cuidad Donde se Ubica: ");
-                escuela.Ciudad = Console.ReadLine();
-                Console.Clear();
-                Console.WriteLine("A Continuacion se Solicitara la Carga de los Cursos...");
-                Console.ReadKey();
-                Console.Clear();
-                do
-                {
-                    var curso = CargarCurso(escuela);
-                    if (curso != null)
-                        escuela.Cursos.Add(curso);
-                    else
-                        break;
-                } while (true);
-                return escuela;
-            }
-
-            private static Curso CargarCurso(Escuela esc)
-            {
-                Curso curso = new Curso();
-                curso.escuela = esc;
-
-                Console.WriteLine("Ingrese el Nombre del Curso/Materia:");
-                curso.Nombre = Console.ReadLine();
-                if (string.IsNullOrWhiteSpace(curso.Nombre)) return null;
-
-                Console.WriteLine("Ingrese los Datos del Profersor del Curso:");
-                curso.AsignarProfesor(CargarProfesor());
-
-                Console.WriteLine("Ingrese los Datos de los Alumnos del Curso:");
-                do
-                {
-                    var alumno = CargarAlumno();
-                    if (alumno != null) curso.InscribirAlumno(alumno);
-                    else break;
-                } while (true);
-                return curso;
-            }
-
-            private static Profesor CargarProfesor()
-            {
-                Profesor profesor = new Profesor();
-                CargarPersona(profesor);
-                Console.WriteLine("Ingrese el Sueldo del Profesor: ");
-                profesor.Sueldo = int.Parse(Console.ReadLine());
-                Console.WriteLine("Ingrese el Titulo del Profesor: ");
-                profesor.Titulo = Console.ReadLine();
-                return profesor;
-            }
-
-            private static Alumno CargarAlumno()
-            {
-                Alumno alumno = new Alumno();
-
-                Console.WriteLine("Ingrese el Nombre del Alumno");
-                alumno.Nombre = Console.ReadLine();
-                if (string.IsNullOrWhiteSpace(alumno.Nombre)) return null;
-                Console.WriteLine("Ingrese el Apellido del Alumno:");
-                alumno.Apellido = Console.ReadLine();
-                Console.WriteLine("Ingrese el Año del Alumno:");
-                alumno.Año = Console.ReadLine();
-                return alumno;
-            }
-
-            private static void CargarPersona(Persona p)
-            {
-                Console.WriteLine("Ingrese el Nombre de la Persona: ");
-                p.Nombre = Console.ReadLine();
-                Console.WriteLine("Ingrese el Apellido de la Persona: ");
-                p.Apellido = Console.ReadLine();
-            }
-
-            private static void MostrarEscuelas(List<Escuela> escuelas)
-            {
-                foreach (var escuela in escuelas)
-                {
-                    Console.WriteLine($"Escuela: {escuela.NombreEsc}");
-                    Console.WriteLine($" Ciudad: {escuela.Ciudad}");
-
-                    Console.WriteLine("  Cursos:");
-                    foreach (var curso in escuela.Cursos)
-                    {
-                        Console.WriteLine($"    {curso.Nombre}");
-                        Console.WriteLine($"      Profesor: {curso.profesor.Nombre}, {curso.profesor.Apellido}");
-
-
-                        foreach (var alumno in curso.alumnos)
-                        {
-                            Console.WriteLine($"        - {alumno.Nombre}, {alumno.Apellido}");
-                        }
-                    }
-                    Console.WriteLine();
-                }
+            Console.WriteLine(MenuIncio);
+            Console.WriteLine("Presione cualquier tecla para continuar...");
             Console.ReadKey();
-            }
+            Console.Clear();
+        }
 
+        private static Escuela CargarEscuela()
+        {
+            Escuela escuela = new Escuela();
 
-            static int MostrarMenu(string[] Opciones, string InitalText = "Seleccione una opción del menu:")
+            Console.WriteLine("Ingrese el Nombre de la Institución: ");
+            escuela.NombreEsc = Console.ReadLine();
+            if (string.IsNullOrWhiteSpace(escuela.NombreEsc)) return null;
+
+            Console.WriteLine("Ingrese la Cuidad Donde se Ubica: ");
+            escuela.Ciudad = Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("A Continuacion se Solicitara la Carga de los Cursos...");
+            Console.ReadKey();
+            Console.Clear();
+            do
             {
-                bool loop = true;
-                int counter = 0;
-                ConsoleKeyInfo Tecla;
+                var curso = CargarCurso(escuela);
+                if (curso != null)
+                    escuela.Cursos.Add(curso);
+                else
+                    break;
+            } while (true);
+            return escuela;
+        }
 
-                Console.CursorVisible = false; // Con esto hacemos que el cursor no se muestre en consola
+        private static Curso CargarCurso(Escuela esc)
+        {
+            Curso curso = new Curso();
+            curso.escuela = esc;
 
-                while (loop)
+            Console.WriteLine("Ingrese el Nombre del Curso/Materia:");
+            curso.Nombre = Console.ReadLine();
+            if (string.IsNullOrWhiteSpace(curso.Nombre)) return null;
+
+            Console.WriteLine("Ingrese los Datos del Profersor del Curso:");
+            curso.AsignarProfesor(CargarProfesor());
+
+            Console.WriteLine("Ingrese los Datos de los Alumnos del Curso:");
+            do
+            {
+                var alumno = CargarAlumno();
+                if (alumno != null) curso.InscribirAlumno(alumno);
+                else break;
+            } while (true);
+            return curso;
+        }
+
+        private static Profesor CargarProfesor()
+        {
+            Profesor profesor = new Profesor();
+            CargarPersona(profesor);
+            Console.WriteLine("Ingrese el Sueldo del Profesor: ");
+            profesor.Sueldo = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese el Titulo del Profesor: ");
+            profesor.Titulo = Console.ReadLine();
+            return profesor;
+        }
+
+        private static Alumno CargarAlumno()
+        {
+            Alumno alumno = new Alumno();
+
+            Console.WriteLine("Ingrese el Nombre del Alumno");
+            alumno.Nombre = Console.ReadLine();
+            if (string.IsNullOrWhiteSpace(alumno.Nombre)) return null;
+            Console.WriteLine("Ingrese el Apellido del Alumno:");
+            alumno.Apellido = Console.ReadLine();
+            Console.WriteLine("Ingrese el Año del Alumno:");
+            alumno.Año = Console.ReadLine();
+            return alumno;
+        }
+
+        private static void CargarPersona(Persona p)
+        {
+            Console.WriteLine("Ingrese el Nombre de la Persona: ");
+            p.Nombre = Console.ReadLine();
+            Console.WriteLine("Ingrese el Apellido de la Persona: ");
+            p.Apellido = Console.ReadLine();
+        }
+
+        private static void MostrarEscuelas(List<Escuela> escuelas)
+        {
+            foreach (var escuela in escuelas)
+            {
+                Console.WriteLine($"Escuela: {escuela.NombreEsc}");
+                Console.WriteLine($" Ciudad: {escuela.Ciudad}");
+
+                Console.WriteLine("  Cursos:");
+                foreach (var curso in escuela.Cursos)
                 {
-                    Console.Clear();
-                    Console.WriteLine(InitalText + System.Environment.NewLine);
-                    string SeleccionActual = string.Empty;
-                    int Destacado = 0;
-
-                    Array.ForEach(Opciones, element =>
+                    Console.WriteLine($"    {curso.Nombre}");
+                    Console.WriteLine($"      Profesor: {curso.profesor.Nombre}, {curso.profesor.Apellido}");
+                    Console.WriteLine($"       Alumnos:");
+                    foreach (var alumno in curso.alumnos)
                     {
-                        if (Destacado == counter)
-                        {
-                            Console.ForegroundColor = ConsoleColor.Yellow;
-                            Console.BackgroundColor = ConsoleColor.Red;
-                            Console.WriteLine(" > " + element + " < ");
-                            Console.ForegroundColor = ConsoleColor.White;
-                            Console.BackgroundColor = ConsoleColor.Black;
-                            SeleccionActual = element;
-                        }
-                        else
-                        {
-                            Console.WriteLine(element);
-                        }
-                        Destacado++;
-                    });
-
-                    Tecla = Console.ReadKey(true);
-                    if (Tecla.Key == ConsoleKey.Enter)
-                    {
-                        loop = false;
-                        break;
-                    }
-                    switch (Tecla.Key)
-                    {
-                        case ConsoleKey.DownArrow:
-                            if (counter < Opciones.Length - 1) counter++;
-                            break;
-                        case ConsoleKey.UpArrow:
-                            if (counter > 0) counter--;
-                            break;
-                        default:
-                            break;
+                        Console.WriteLine($"          - {alumno.Nombre}, {alumno.Apellido}");
                     }
                 }
-                return counter;
+                Console.WriteLine();
             }
+            Console.ReadKey();
+        }
+
+
+        static int MostrarMenu(string[] Opciones, string InitalText = "Seleccione una opción del menu:")
+        {
+            bool loop = true;
+            int counter = 0;
+            ConsoleKeyInfo Tecla;
+
+            Console.CursorVisible = false; // Con esto hacemos que el cursor no se muestre en consola
+
+            while (loop)
+            {
+                Console.Clear();
+                Console.WriteLine(InitalText + System.Environment.NewLine);
+                string SeleccionActual = string.Empty;
+                int Destacado = 0;
+
+                Array.ForEach(Opciones, element =>
+                {
+                    if (Destacado == counter)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Yellow;
+                        Console.BackgroundColor = ConsoleColor.Red;
+                        Console.WriteLine(" > " + element + " < ");
+                        Console.ForegroundColor = ConsoleColor.White;
+                        Console.BackgroundColor = ConsoleColor.Black;
+                        SeleccionActual = element;
+                    }
+                    else
+                    {
+                        Console.WriteLine(element);
+                    }
+                    Destacado++;
+                });
+
+                Tecla = Console.ReadKey(true);
+                if (Tecla.Key == ConsoleKey.Enter)
+                {
+                    loop = false;
+                    break;
+                }
+                switch (Tecla.Key)
+                {
+                    case ConsoleKey.DownArrow:
+                        if (counter < Opciones.Length - 1) counter++;
+                        break;
+                    case ConsoleKey.UpArrow:
+                        if (counter > 0) counter--;
+                        break;
+                    default:
+                        break;
+                }
+            }
+            return counter;
         }
     }
+}
